252e9b36f401c59454707c8414cba5138da424d3
agateee1234 <119411053+agateee1234@users.noreply.github.com>
2023-03-31 15:36:52 +0300

    Pievienoju darbību funkcijas

--- script.js
+++ script.js
@@ -2,6 +2,22 @@ const input1 = document.querySelector('.js-teksts')
 
 const plusButton = document.querySelector('.js-plus')
 
+const Add = (skaitlis1, skaitlis2) => {
+   return skaitlis1 + skaitlis2
+}
 
-console.log(input1)
-console.log(plusButton)
+const Subtract = (skaitlis1,skaitlis2) => {
+    return skaitlis1 - skaitlis2
+}
+
+const Mulitply = (skaitlis1, skaitlis2) => {
+       return skaitlis1 * skaitlis2
+}
+
+const Divide = (skaitlis1, skaitlis2) => {
+       return skaitlis1 / skaitlis2
+}
+
+plusButton.addEventListener('click', () => {
+    console.log("Mani nospieda")
+})
