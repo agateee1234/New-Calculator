fdfd7a13a39ccf24f2bee8464f490ec5ac1f8688
agateee1234 <119411053+agateee1234@users.noreply.github.com>
2023-04-11 12:36:01 +0300

    Pabeidzām lapas funkcionalitāti

--- script.js
+++ script.js
@@ -7,22 +7,45 @@ const divideButton = document.querySelector('.js-divide');
 const result = document.querySelector('.js-result');
 
 const Add = (skaitlis1, skaitlis2) => {
-   return Number(skaitlis1) + Number(skaitlis2)
+    return Number(skaitlis1) + Number(skaitlis2)
 }
 
-const Subtract = (skaitlis1,skaitlis2) => {
-    return skaitlis1 - skaitlis2
+const Subtract = (skaitlis1, skaitlis2) => {
+    return Number(skaitlis1) - Number(skaitlis2)
 }
 
-const Mulitply = (skaitlis1, skaitlis2) => {
-       return skaitlis1 * skaitlis2
+const Multiply = (skaitlis1, skaitlis2) => {
+    return Number(skaitlis1) * Number(skaitlis2)
 }
 
 const Divide = (skaitlis1, skaitlis2) => {
-       return skaitlis1 / skaitlis2
+    return Number(skaitlis1) / Number(skaitlis2)
 }
 
 plusButton.addEventListener('click', () => {
     result.innerHTML = Add(input1.value, input2.value)
+
+    input1.value = ""
+    input2.value = ""
 })
 
+minusButton.addEventListener('click', () => {
+    result.innerHTML = Subtract(input1.value, input2.value)
+
+    input1.value = ""
+    input2.value = ""
+})
+
+multiplyButton.addEventListener('click', () => {
+    result.innerHTML = Multiply(input1.value, input2.value)
+
+    input1.value = ""
+    input2.value = ""
+})
+
+divideButton.addEventListener('click', () => {
+    result.innerHTML = Divide(input1.value, input2.value)
+
+    input1.value = ""
+    input2.value = ""
+})
